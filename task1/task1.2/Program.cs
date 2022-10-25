75d52c37beee101b0fd62d369a579ba0a837a1d4
Seif El-Din Sweilam <saifsweelam@gmail.com>
2022-10-25 21:42:17 +0200

    Implement task 1.2

--- task1/task1.2/Program.cs
+++ task1/task1.2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Task 1.2
+ *
+ * Write a program that reads from the console the radius
+ * of a circle and prints its perimeter and area
+ */
+
+const double PIE = 22.0 / 7.0;
+Console.WriteLine("Enter Radius:");
+double radius = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Perimeter: {2*PIE*radius}");
+Console.WriteLine($"Area: {PIE*radius*radius}");
